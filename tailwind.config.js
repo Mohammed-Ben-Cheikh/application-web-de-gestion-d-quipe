dd158b63e400decf58e29ed5ee3bd05ff484b5d1
mohammed-ben-cheikh <bencheikh.official@gmail.com>
2024-12-03 21:44:19 +0100

    Initialize project with Tailwind CSS setup, add configuration files, and create initial styles. Include player data in JSON format and various image assets for the application.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html","./{js,page}/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Comfortaa', 'sans-serif'],
+      },
+      fontSize: {
+        'custom-32': '32px',
+        'custom-15': '15px',
+        'custom-20': '20px',
+        'custom-16': '16px'
+      }
+    },
+  },
+  plugins: [],
+}
+
