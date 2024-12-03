0b3b0374ae054782468dc2e23d75c751597f5555
mohammed-ben-cheikh <bencheikh.official@gmail.com>
2024-12-03 22:29:35 +0100

    Add player display functionality in app.js</message>
    <message>
    - Implemented a new function `afficherJoueurs` to dynamically generate and display player cards based on their data.
    - Enhanced the visual representation of players, including goalkeepers and other positions, with detailed statistics and images.
    - Improved user interaction by allowing clicks on player cards to trigger specific functions related to player actions.
    - This update enhances the EA FC 25 Squad Builder's user experience by providing a more interactive and visually appealing way to view player information. #9

--- js/app.js
+++ js/app.js
@@ -128,6 +128,148 @@ function fetchEtStockerJoueursParPosition() {
     }
 }
 
+// Fonction pour afficher les joueurs
+function afficherJoueurs(players) {
+    const container = document.getElementById('players-container');
+    let cardsHTML = '';
 
+    players.forEach(player => {
+        if (player.position === "GK") {
+            cardsHTML += `
+<div data="${player.name},${player.position},${player.club},${player.nationality}" onclick="joueurDansTerrainFunction('${player.name}','${player.position}')" class="relative w-[80%] p-5">
+    <div class="relative bg-black rounded-lg shadow-lg h-[250px] transition-transform hover:scale-105 cursor-pointer">
+        <div
+            class="absolute inset-0 bg-gradient-to-r from-green-500 via-yellow-400 to-red-500 opacity-20 rounded-lg group-hover:blur-lg transition-all duration-300">
+        </div>
+        <div class="relative p-4 h-full flex flex-col justify-around">
+            <div class="flex justify-between items-center">
+                <div class="bg-yellow-500 text-white rounded-full w-12 h-12 flex items-center justify-center">
+                    <span class="text-lg font-bold">${player.rating}</span>
+                </div>
+                <!-- Photo du joueur -->
+                <div class="flex justify-center -mt-2">
+                    <img src="${player.photo}" alt="${player.name}"
+                        class="w-20 h-20 rounded-full border-4 border-white object-cover">
+                </div>
+                <div class="bg-yellow-500 text-white rounded-full w-12 h-12 flex items-center justify-center">
+                    <span class="text-lg font-bold">${player.position}</span>
+                </div>
+            </div>
+            <div class="text-center">
+                <h2 class="text-xl font-bold text-white mb-2">${player.name}</h2>
+            </div>
+            <div class="flex justify-center items-center gap-3">
+                <img src="${player.flag}" alt="Drapeau" class="w-8 h-5 rounded shadow">
+                <img src="${player.logo}" alt="Club" class="w-8 h-8 rounded shadow">
+            </div>
+            <div class="flex justify-around flex-col gap-4">
+                <div class="flex justify-around">
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">DIV</span>
+                        <span class="text-white block text-sm">${player.diving}</span>
+                    </div>
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">HAN</span>
+                        <span class="text-white block text-sm">${player.handling}</span>
+                    </div>
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">KIC</span>
+                        <span class="text-white block text-sm">${player.kicking}</span>
+                    </div>
+                </div>
+                <div class="flex justify-around">
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">REF</span>
+                        <span class="text-white block text-sm">${player.reflexes}</span>
+                    </div>
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">SPD</span>
+                        <span class="text-white block text-sm">${player.speed}</span>
+                    </div>
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">POS</span>
+                        <span class="text-white block text-sm">${player.positioning}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`;
+        } else {
+            cardsHTML += `
+<div data="${player.name},${player.position},${player.club},${player.nationality}" onclick="joueurDansTerrainFunction('${player.name}','${player.position}')" class="relative w-[80%] p-5">
+    <div class="relative bg-black rounded-lg shadow-lg h-[250px] transition-transform hover:scale-105 cursor-pointer">
+        <!-- Effet de gradient -->
+        <div
+            class="absolute inset-0 bg-gradient-to-r from-green-500 via-yellow-400 to-red-500 opacity-20 rounded-lg group-hover:blur-lg transition-all duration-300">
+        </div>
+        <!-- Contenu principal -->
+        <div class="relative p-4 h-full flex flex-col justify-around">
+            <!-- En-tête avec rating et position -->
+            <div class="flex justify-between items-center">
+                <div class="bg-yellow-500 text-white rounded-full w-12 h-12 flex items-center justify-center">
+                    <span class="text-lg font-bold">${player.rating}</span>
+                </div>
+                <!-- Photo du joueur -->
+                <div class="flex justify-center -mt-2">
+                    <img src="${player.photo}" alt="${player.name}"
+                        class="w-20 h-20 rounded-full border-4 border-white object-cover">
+                </div>
+                <div class="bg-yellow-500 text-white rounded-full w-12 h-12 flex items-center justify-center">
+                    <span class="text-lg font-bold">${player.position}</span>
+                </div>
+            </div>
+            <!-- Informations du joueur -->
+            <div class="text-center">
+                <div class="flex flex-col items-center">
+                    <h2 class="text-xl font-bold text-white mb-2">${player.name}</h2>
+                </div>
+                <div class="flex justify-center items-center gap-3">
+                    <img src="${player.flag}" alt="Drapeau" class="w-8 h-5 rounded shadow">
+                    <img src="${player.logo}" alt="Club" class="w-8 h-8 rounded shadow">
+                </div>
+            </div>
+            <!-- Statistiques -->
+            <div class="flex justify-around flex-col gap-4">
+                <div class="flex justify-around">
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">PAC</span>
+                        <span class="text-white block text-sm">${player.pace}</span>
+                    </div>
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">SHO</span>
+                        <span class="text-white block text-sm">${player.shooting}</span>
+                    </div>
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">PAS</span>
+                        <span class="text-white block text-sm">${player.passing}</span>
+                    </div>
+                </div>
+                <div class="flex justify-around">
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">DRI</span>
+                        <span class="text-white block text-sm">${player.dribbling}</span>
+                    </div>
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">DEF</span>
+                        <span class="text-white block text-sm">${player.defending}</span>
+                    </div>
+                    <div class="flex flex-col items-center">
+                        <span class="text-white text-sm font-bold">PHY</span>
+                        <span class="text-white block text-sm">${player.physical}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+            `;
+        }
+
+
+    });
+
+    container.innerHTML = cardsHTML
+}
 
 
